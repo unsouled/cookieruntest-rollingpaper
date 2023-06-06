16296c8ae12885579a5fe5293fa64f2474d99173
Ki-yong Kim <unsouled1@gmail.com>
2023-06-07 07:46:12 +0900

    change cookie -> cookieCharacter

---
+++ backend/src/api/cookie-character/routes/cookie-character.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cookie-character router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::cookie-character.cookie-character');
---
+++ backend/src/api/cookie-character/services/cookie-character.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cookie-character service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::cookie-character.cookie-character');
