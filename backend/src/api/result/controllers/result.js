68c334a5df1501e4904d3d56ca2aa9ce937e73d6
Ki-yong Kim <unsouled1@gmail.com>
2023-06-07 08:13:51 +0900

    delete result

--- backend/src/api/result/controllers/result.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * result controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::result.result');
--- backend/src/api/result/services/result.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * result service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::result.result');
