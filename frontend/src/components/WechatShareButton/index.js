46eaca9a1b574ddc6fbaaa3d85bd10828bb56915
Ki-yong Kim <unsouled1@gmail.com>
2023-06-23 18:32:55 +0900

    wechat 공유 기능 추가

--- frontend/src/components/WechatShareButton/index.js
+++ frontend/src/components/WechatShareButton/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import styled from '@emotion/styled';
 import ShareButton from '/src/components/ShareButton';
 import { StaticImage } from 'gatsby-plugin-image';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const StyledShareButton = styled(ShareButton)`
   background: url('/images/wechat@3x.png') no-repeat center center #2DC100;
   background-size: 21px 18px;
 `;
 
-export default (props) => (
-  <StyledShareButton {...props} />
+export default ({ url, ...props }) => (
+  <CopyToClipboard text={url} onCopy={() => {
+    window.location.href = 'weixin://dl/chat';
+  }}>
+    <StyledShareButton {...props} />
+  </CopyToClipboard>
 );
