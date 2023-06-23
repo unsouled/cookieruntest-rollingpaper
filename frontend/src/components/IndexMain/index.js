79f676b526aead80b01fd28284e41266a803e0df
Ki-yong Kim <unsouled1@gmail.com>
2023-06-24 00:17:43 +0900

    update ui

--- frontend/src/components/IndexMain/index.js
+++ frontend/src/components/IndexMain/index.js
@@ -126,96 +126,168 @@ const IndexMain = ({ showModal, banner, lang = 'en', mid }) => {
   }, []);
 
   return (
-    <CustomLayout>
-      <Banner onClick={() => window.location.href='https://ckie.run/test '}>
-        <GatsbyImage image={bannerImageData} width="100%" height="100%" />
-      </Banner>
-      <div css={{ display: 'flex', maxWidth: '320px', margin: '0 auto' }}>
-        <div css={{ width: '100px', justifyContent: 'flex', padding: '20px 0' }}>
-          <StaticImage 
-            width={25}
-            height={25}
-            src="../../images/img-icon-cro@3x.png" 
-            alt=""
-            placeholder="none"
-          />
-        </div>
-        <div css={{ flex:1 }}>
-        </div>
-        <div css={{ width: '100px', justifyContent: 'flex-end', display: 'flex', padding: '20px 0' }}>
-          <span onClick={showModal} css={{ textAlign: 'left', fontSize: '20px', width: '84px', display: 'inline-block', fontWeight: 700 }}>
-            {languagesByLocale[lang]}
-          </span>
-          <span css={{ fontSize: '20px', lineHeight: '20px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <>
+      <CustomLayout css={{ position: 'absolute', top: 0 }}>
+        <Banner onClick={() => window.location.href='https://ckie.run/test '}>
+          <GatsbyImage image={bannerImageData} width="100%" height="100%" />
+        </Banner>
+        <div css={{ display: 'flex', maxWidth: '320px', margin: '0 auto' }}>
+          <div css={{ width: '100px', justifyContent: 'flex', padding: '20px 0' }}>
             <StaticImage 
-              src="../../images/polygon@3x.png" width={10} height={8} 
+              width={25}
+              height={25}
+              src="../../images/img-icon-cro@3x.png" 
+              alt=""
               placeholder="none"
             />
-          </span>
+          </div>
+          <div css={{ flex:1 }}>
+          </div>
+          <div css={{ width: '100px', justifyContent: 'flex-end', display: 'flex', padding: '20px 0' }}>
+            <span onClick={showModal} css={{ textAlign: 'left', fontSize: '20px', width: '84px', display: 'inline-block', fontWeight: 700 }}>
+              {languagesByLocale[lang]}
+            </span>
+            <span css={{ fontSize: '20px', lineHeight: '20px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <StaticImage 
+                src="../../images/polygon@3x.png" width={10} height={8} 
+                placeholder="none"
+              />
+            </span>
+          </div>
+        </div>
+        <Main>
+          <div css={{ flex: 1, display: 'flex', flexDirection: 'column', position: 'relative' }}>
+            <StaticImage 
+              css={{ position: 'absolute', top: 95, left: '-55px' }}
+              width={112}
+              height={117}
+              src="../../images/img-cookie-1@3x.png" 
+              alt=""
+              placeholder="none"
+            />
+            <StaticImage 
+              css={{ position: 'absolute', top: 195, right: '-35px' }}
+              width={83}
+              height={86}
+              src="../../images/img-cookie-2@3x.png" 
+              alt=""
+              placeholder="none"
+            />
+
+            <Description>
+              <div>{localizedMessages['description1']}</div>
+              <div className="description2" style={{ textAlign: 'left' }}>
+                <StaticImage 
+                  className="xxx"
+                  style={{ left: -10, top: -8, marginTop: 20, marginRight: 0, transform: 'rotate(8.29deg)' }}
+                  width={77}
+                  height={14}
+                  src="../../images/img-xxx@3x.png" 
+                  alt=""
+                  placeholder="none"
+                />
+                {localizedMessages['description2']}
+                <StaticImage 
+                  style={{ left: 6, top: -14 }}
+                  width={41}
+                  height={37}
+                  src="../../images/img-heart@3x.png" 
+                  alt=""
+                  placeholder="none"
+                />
+
+              </div>
+            </Description>
+            <Title className="title">
+              {localizedMessages['titleRich']}
+            </Title>
+          </div>
+          <Buttons>
+            <ParticipantsText>
+              {sf(localizedMessages['participantsText'], { participants: new Intl.NumberFormat(lang, { maximumSignificantDigits: 3 }).format(counter) })}
+            </ParticipantsText>
+            <StartButton onClick={handleStart}>
+              {localizedMessages['startText']}
+            </StartButton>
+            <CopyToClipboard text={process.env.GATSBY_HOST} onCopy={() => { alert(localizedMessages['alertCopied']) }}>
+              <CopyButton>
+                {localizedMessages['copyLinkText']}
+              </CopyButton>
+            </CopyToClipboard>
+          </Buttons>
+        </Main>
+      </CustomLayout>
+      <div css={{ position: 'absolute', top: 0, width: '100%', height: '100%', zIndex: -100 }}>
+        <div css={{ position: 'relative', width: '310px', height: '470px', margin: '0 auto' }}>
+          <StaticImage 
+            css={{ position: 'absolute', top: 80, right: 120 }}
+            width={16}
+            height={21}
+            src="../../images/img-idx-part@3x.png" 
+            alt=""
+            placeholder="none"
+          />
+          <StaticImage 
+            css={{ position: 'absolute', top: 210, right: -40 }}
+            src="../../images/img-idx-part2@3x.png" 
+            width={16}
+            height={12}
+            alt=""
+            placeholder="none"
+          />
+          <StaticImage 
+            css={{ position: 'absolute', top: 120, left: 68 }}
+            src="../../images/img-idx-part3@3x.png" 
+            width={9}
+            height={18}
+            alt=""
+            placeholder="none"
+          />
+          <StaticImage 
+            css={{ position: 'absolute', top: 490, left: -16 }}
+            src="../../images/img-idx-part4@3x.png" 
+            width={9}
+            height={18}
+            alt=""
+            placeholder="none"
+          />
+          <StaticImage 
+            css={{ position: 'absolute', top: 626, left: 56 }}
+            src="../../images/img-idx-part5@3x.png" 
+            width={32}
+            height={37}
+            alt=""
+            placeholder="none"
+          />
+          <StaticImage 
+            css={{ position: 'absolute', top: 610, right: 80 }}
+            src="../../images/img-idx-part6@3x.png" 
+            width={9}
+            height={18}
+            alt=""
+            placeholder="none"
+          />
+          <StaticImage 
+            css={{ position: 'absolute', top: 460, right: -36 }}
+            src="../../images/img-idx-part7@3x.png" 
+            width={45}
+            height={28}
+            alt=""
+            placeholder="none"
+          />
+          <StaticImage 
+            css={{ position: 'absolute', top: 380, left: -32 }}
+            src="../../images/img-idx-part8@3x.png" 
+            width={43}
+            height={28}
+            alt=""
+            placeholder="none"
+          />
+
+
         </div>
       </div>
-      <Main>
-        <div css={{ flex: 1, display: 'flex', flexDirection: 'column', position: 'relative' }}>
-          <StaticImage 
-            css={{ position: 'absolute', top: 95, left: '-55px' }}
-            width={112}
-            height={117}
-            src="../../images/img-cookie-1@3x.png" 
-            alt=""
-            placeholder="none"
-          />
-          <StaticImage 
-            css={{ position: 'absolute', top: 195, right: '-35px' }}
-            width={83}
-            height={86}
-            src="../../images/img-cookie-2@3x.png" 
-            alt=""
-            placeholder="none"
-          />
-
-          <Description>
-            <div>{localizedMessages['description1']}</div>
-            <div className="description2" style={{ textAlign: 'left' }}>
-              <StaticImage 
-                className="xxx"
-                style={{ left: -10, top: -8, marginTop: 20, marginRight: 0, transform: 'rotate(8.29deg)' }}
-                width={77}
-                height={14}
-                src="../../images/img-xxx@3x.png" 
-                alt=""
-                placeholder="none"
-              />
-              {localizedMessages['description2']}
-              <StaticImage 
-                style={{ left: 6, top: -14 }}
-                width={41}
-                height={37}
-                src="../../images/img-heart@3x.png" 
-                alt=""
-                placeholder="none"
-              />
-
-            </div>
-          </Description>
-          <Title className="title">
-            {localizedMessages['titleRich']}
-          </Title>
-        </div>
-        <Buttons>
-          <ParticipantsText>
-            {sf(localizedMessages['participantsText'], { participants: new Intl.NumberFormat(lang, { maximumSignificantDigits: 3 }).format(counter) })}
-          </ParticipantsText>
-          <StartButton onClick={handleStart}>
-            {localizedMessages['startText']}
-          </StartButton>
-          <CopyToClipboard text={process.env.GATSBY_HOST} onCopy={() => { alert(localizedMessages['alertCopied']) }}>
-            <CopyButton>
-              {localizedMessages['copyLinkText']}
-            </CopyButton>
-          </CopyToClipboard>
-        </Buttons>
-      </Main>
-    </CustomLayout>
+    </>
   );
 }
 
